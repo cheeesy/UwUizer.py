2f48f4ba51a868da47ac1c7de01ec4359de0927e
cheeesy <cheeesy@aaathats3as.com>
2019-11-23 14:41:46 +0100

    love -> wuv

--- UwUizer.py
+++ UwUizer.py
@@ -13,11 +13,12 @@ def UwUizer(uwu):
     uwu = uwu.replace("man.", "boyfriend.")
     uwu = uwu.replace(" man", " boyfriend") #... uwu
     uwu = uwu.replace("iend", "end")
+    uwu = uwu.replace("v", "w")
+    uwu = uwu.replace("lowe", "wuv")
     uwu = uwu.replace(".", ". uwu") # an UwU after every sentence
     uwu = uwu.replace("?", "? >~<") # a hiding cute >~< after every question
     uwu = uwu.replace("!", "! ÒwÓ")
     uwu = uwu.replace("r", "w") # Replace hard r's with weak w's!(๑•̀ㅁ•́๑)
-    uwu = uwu.replace("v", "w")
     uwu = uwu.replace("awe", "awwe") # Replace are with awwe UwU
     uwu = uwu.replace("na", "nya") # Replace na with nya
     uwu = uwu.replace("l", "w")

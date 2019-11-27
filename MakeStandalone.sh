b182c7d9e08fa6da99b9c0eadba3baa7af130795
cheeesy <cheeesy@aaathats3as.com>
2019-11-27 20:25:51 +0100

    Added some new rules; also emotionsgit add *!

--- MakeStandalone.sh
+++ MakeStandalone.sh
@@ -2,22 +2,10 @@
 
 # Standalone generator for cheeesy/UwUizer.py
 
-sed -i 's;#!/usr/bin/env python3;#bigdumb;g' UwUizer.py
-sed -i 's;NotYetUwUed="Placeholder text?";#NotYetUwUed;g' UwUizer.py
-
 standalonePath="standalone.py"
 
-echo "#!/usr/bin/env python3" > $standalonePath
-echo "" >> $standalonePath
-echo 'NotYetUwUed=input("What wowd do yowouwu want towo uwu-ize? ")' >> $standalonePath
-echo "" >> $standalonePath
+cp UwUizer.py $standalonePath
 
-echo "$(cat $standalonePath UwUizer.py)" > $standalonePath
-
-sed -i "s;#bigdumb;;g" $standalonePath
-sed -i "s;#NotYetUwUed;;g" $standalonePath
-
-sed -i 's;#bigdumb;#!/usr/bin/env python3;g' UwUizer.py
-sed -i 's;#NotYetUwUed;NotYetUwUed="Placeholder text?";g' UwUizer.py
+sed -i 's;"Placeholder text?";input("What wowowd do yowouwu want towo uwu-ize? ");g' $standalonePath
 
 chmod +x $standalonePath
--- UwUizer.py
+++ UwUizer.py
@@ -10,8 +10,12 @@ NotYetUwUed="Placeholder text?"
 UwUed=""
 
 def UwUizer(uwu):
+    uwu = uwu.replace("prettier", ";prettier")
+    uwu = uwu.replace("your", ";your")
     uwu = uwu.replace("you", "chu")
-    uwu = uwu.replace("Idiot", "Baka")
+    uwu = uwu.replace(";chur", "your")
+    uwu = uwu.replace("very", ";very")
+    uwu = uwu.replace("idiot", "Baka! >~<")
     uwu = uwu.replace("say", "sej")
     uwu = uwu.replace("ing", "in")
     uwu = uwu.replace("man.", "boyfriend.")
@@ -20,8 +24,13 @@ def UwUizer(uwu):
     uwu = uwu.replace("v", "w")
     uwu = uwu.replace("lowe", "wuv")
     uwu = uwu.replace(".", ". uwu") # an UwU after every sentence
-    uwu = uwu.replace("?", "? >~<") # a hiding cute >~< after every question
-    uwu = uwu.replace("!", "! ÒwÓ")
+    uwu = uwu.replace("!;", ";! >~<") # Emotions
+    uwu = uwu.replace("!a", ";! ÒwÓ")
+    uwu = uwu.replace("!e", ";! (Ò﹏Ó ╬)")
+    if(uwu.find(";!") == -1):
+        uwu = uwu.replace("!", "! OWO")
+    uwu = uwu.replace(";!", "!")
+    uwu = uwu.replace("?", "? ówò")
     uwu = uwu.replace("r", "w") # Replace hard r's with weak w's!(๑•̀ㅁ•́๑)
     uwu = uwu.replace("awe", "awwe") # Replace are with awwe UwU
     uwu = uwu.replace("na", "nya") # Replace na with nya
@@ -32,9 +41,18 @@ def UwUizer(uwu):
     uwu = uwu.replace("gay", "gei")
     uwu = uwu.replace("howd", "hold") # Hold may stay
     uwu = uwu.replace("oo", "owo")
+    uwu = uwu.replace("nice", "nais")
+    uwu = uwu.replace(";wewy", "vewwy")
+    uwu = uwu.replace(";pwettiew", "pwettier")
     if (uwu[-1] == 'Ó'):
         uwu += ''
+    elif (uwu[-1] == 'O'):
+        uwu += ''
     elif (uwu[-1] == '<'):
+        uwu += ''
+    elif (uwu[-1] == ')'):
+        uwu += ''
+    elif (uwu[-1] == 'u'):
         uwu += ''
     else:
         uwu += ' uwu~'
@@ -42,5 +60,5 @@ def UwUizer(uwu):
     
 
 if __name__ == '__main__':
-    UwUed = UwUizer(NotYetUwUed)
+    UwUed = UwUizer(NotYetUwUed.lower())
     print(UwUed)
